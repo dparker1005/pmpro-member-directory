106e99f16875c1896fa61b1af93a154337e554c4
Andrew Lima <andrew@arctek.co.za>
2025-10-21 10:56:09 +0200

    Show warning message on profile page.
    * ENHANCEMENT: Show a warning if we ever get to a profile page but fail to get the user object of said user. It should never reach to this, but it's possible.

--- templates/profile.php
+++ templates/profile.php
@@ -46,6 +46,17 @@ function pmpromd_profile_shortcode( $atts, $content=null, $code="" ) {
 	// Get the user for this profile.
 	$pu = pmpromd_get_user( $user_id );
 
+	// If we're unable to get a user, show a message (to admins).
+	if ( empty( $pu ) || ! is_object( $pu ) ) {
+		$member_not_found = esc_html__( 'Unable to retrieve member profile.', 'pmpro-member-directory' );
+		if ( $directory_url ) {
+			$member_not_found .= ' <a href="' . esc_url( $directory_url ) . '">' . esc_html__( 'Go back to Directory', 'pmpro-member-directory' ) . '</a>';
+		} else {
+			$member_not_found .= ' ' . esc_html__( 'Please contact the site administrator for assistance.', 'pmpro-member-directory' );
+		}
+		return $member_not_found;
+	}
+
 	// Did they use level instead of levels?
 	if ( empty( $levels ) && ! empty( $level ) ) {
 		$levels = $level;
