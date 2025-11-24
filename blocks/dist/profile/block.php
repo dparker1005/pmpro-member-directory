0fe1ed6544f365205b80f85babb53151762b0510
David Parker <dlparker1005@gmail.com>
2025-11-24 10:43:57 -0500

    Only enqueue editor scripts when editing a page or post

--- blocks/dist/profile/block.php
+++ blocks/dist/profile/block.php
@@ -13,17 +13,28 @@ if ( ! function_exists( 'register_block_type' ) ) {
 
 // Register block type.
 function pmpromd_register_profile_block() {
+	register_block_type( 'pmpro-member-directory/profile', array(
+		'editor_script' => 'pmpromd-profile-block',
+		'render_callback' => 'pmpromd_profile_shortcode'
+	) );
+}
+add_action( 'init', 'pmpromd_register_profile_block' );	
+
+/**
+ * Register block editor scripts and styles.
+ *
+ * @since TBD
+ */
+function pmpromd_enqueue_profile_block_editor_assets() {
+	// Only load on post and page edit screens.
+	if ( ! in_array( get_post_type(), array( 'page', 'post' ) ) ) {
+		return;
+	}
 
 	wp_register_script( 
 		'pmpromd-profile-block', 
 		plugins_url( 'block.build.js', __FILE__ ), 
 		array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api', 'wp-editor', 'pmpro_admin' )
 	);
-
-	register_block_type( 'pmpro-member-directory/profile', array(
-		'editor_script' => 'pmpromd-profile-block',
-		'render_callback' => 'pmpromd_profile_shortcode'
-	) );
-
 }
-add_action( 'init', 'pmpromd_register_profile_block' );	
+add_action( 'enqueue_block_editor_assets', 'pmpromd_enqueue_profile_block_editor_assets' );
--- blocks/src/directory/block.php
+++ blocks/src/directory/block.php
@@ -9,17 +9,29 @@ if ( ! function_exists( 'register_block_type' ) ) {
 
 // Register block type.
 function pmpromd_register_directory_block() {
-
-	wp_register_script( 
-		'pmpromd-directory-block', 
-		plugins_url( 'block.build.js', __FILE__ ), 
-		array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api', 'wp-editor', 'pmpro_admin' )
-	);
-
 	register_block_type( 'pmpro-member-directory/directory', array(
 		'editor_script' => 'pmpromd-directory-block',
 		'render_callback' => 'pmpromd_shortcode'
 	) );
 
 }
-add_action( 'init', 'pmpromd_register_directory_block' );
+add_action( 'enqueue_block_editor_assets', 'pmpromd_register_directory_block' );
+
+/**
+ * Register block editor scripts and styles.
+ *
+ * @since TBD
+ */
+function pmpromd_enqueue_directory_block_editor_assets() {
+	// Only load on post and page edit screens.
+	if ( ! in_array( get_post_type(), array( 'page', 'post' ) ) ) {
+		return;
+	}
+
+	wp_register_script( 
+		'pmpromd-directory-block', 
+		plugins_url( 'block.build.js', __FILE__ ), 
+		array( 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api', 'wp-editor', 'pmpro_admin' )
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'pmpromd_enqueue_directory_block_editor_assets' );
